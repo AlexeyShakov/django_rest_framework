0113db5332cb1d7aa4bf165583c90612f248c2bd
AlexeyShakov <alexei_96@inbox.ru>
2022-08-08 18:27:21 +0300

    detailed actions of serializing model instance to JSON format

--- drf_site/women/serializers.py
+++ drf_site/women/serializers.py
@@ -1,7 +1,23 @@
-from rest_framework import serializers
+from rest_framework import serializers # serializere converts python objects to JSON and vice versa
+from rest_framework.renderers import JSONRenderer
+
 from .models import Women
 
-class WomenSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Women
-        fields = ("title", "cat_id")
+
+class WomenModel:
+    def __init__(self, title, content):
+        self.title = title
+        self.content = content
+
+# We have to have the same attributes in Serializer class as attributes in the model class
+class WomenSerializer(serializers.Serializer):
+    title = serializers.CharField(max_length=255)
+    content = serializers.CharField()
+
+
+def encode():
+    model = WomenModel("Анджелина Джоли", "Анджелина Джоли")
+    model_serialized = WomenSerializer(model)
+    print(model_serialized.data, "Тип результата:", type(model_serialized.data), sep="\n")
+    json = JSONRenderer().render(model_serialized.data)
+    print(json)
