10e4edd28939a81b93ef08628a9a1b374482e170
AlexeyShakov <alexei_96@inbox.ru>
2022-08-09 00:31:15 +0300

    Implemented the method allowing to create own not standart from SimpleRouter endpoints; implemented the method allowing to output only certain amount of data

--- drf_site/women/views.py
+++ drf_site/women/views.py
@@ -1,8 +1,9 @@
 from django.forms import model_to_dict
 from django.shortcuts import render
 from rest_framework import generics, viewsets
+from rest_framework.decorators import action
 from rest_framework.response import Response
-from .models import Women
+from .models import Women, Category
 from .serializers import WomenSerializer
 from rest_framework.views import APIView
 
@@ -12,6 +13,20 @@ class WomenViewSet(viewsets.ModelViewSet):
     queryset = Women.objects.all()
     serializer_class = WomenSerializer
 
+    # This method allows us to get only certain data from the DB
+    def get_queryset(self):
+        # If we don't do that then when we wanna get the certain post we will get the error. So
+        pk = self.kwargs.get("pk")
+        if not pk:
+            return Women.objects.all()[:3]
+        return Women.objects.filter(pk=pk)
+
+    # This decorator is needed to create new endpoints for our Router class. It will have the URL:
+    # "api/v1/women/category/"
+    @action(methods=["get"], detail=True)
+    def category(self, request, pk=None):
+        cats = Category.objects.get(pk=pk)
+        return Response({"cats": cats.name})
 
 # # Old version with repeating code
 
