9419cc70cbc09951eb7edfd2f0d2099eb2aa5609
AlexeyShakov <alexei_96@inbox.ru>
2022-08-08 13:24:23 +0300

    added django_rest_framework app; created view for API and connected to the url

--- drf_site/women/views.py
+++ drf_site/women/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.response import Response
+from .models import Women
+from .serializers import WomenSerializer
+from rest_framework.views import APIView
 
 # Create your views here.
+
+class WomenApiView(APIView):
+    # This method handles GET requests
+    def get(self, request):
+        # Response converts a dictionary into JSON
+        return Response({'title': 'Анджолина Джоли'})
+
+# class WomenApiView(generics.ListAPIView):
+#     # Takes all data from the DB table
+#     queryset = Women.objects.all()
+#     serializer_class = WomenSerializer
