50c1a9fb480470f9b5ac0c1a57c48dc83bb8a649
AlexeyShakov <alexei_96@inbox.ru>
2022-08-08 12:55:09 +0300

    Created superuser and added new objects to the DB

--- drf_site/women/admin.py
+++ drf_site/women/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Women, Category
 
-# Register your models here.
+
+# Register our models so that they are available at the admin endpoint
+admin.site.register(Women)
+admin.site.register(Category)
