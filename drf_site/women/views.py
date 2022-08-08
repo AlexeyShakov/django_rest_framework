c34bd3e774e27f8cdd6977f052ab3313a8dea6aa
AlexeyShakov <alexei_96@inbox.ru>
2022-08-08 13:52:37 +0300

    Implemented API without a serializer just with get and post methods in a class

--- drf_site/women/views.py
+++ drf_site/women/views.py
@@ -1,3 +1,4 @@
+from django.forms import model_to_dict
 from django.shortcuts import render
 from rest_framework import generics
 from rest_framework.response import Response
@@ -10,8 +11,18 @@ from rest_framework.views import APIView
 class WomenApiView(APIView):
     # This method handles GET requests
     def get(self, request):
+        all_data = Women.objects.all().values()
         # Response converts a dictionary into JSON
-        return Response({'title': 'Анджолина Джоли'})
+        return Response({'posts': list(all_data)})
+
+    def post(self, request):
+        new_data = Women.objects.create(
+            title=request.data["title"],
+            content=request.data["content"],
+            cat_id=request.data["cat_id"]
+        )
+        # model_to_dict converts django model object to a dictionary
+        return Response({"post": model_to_dict(new_data)})
 
 # class WomenApiView(generics.ListAPIView):
 #     # Takes all data from the DB table
